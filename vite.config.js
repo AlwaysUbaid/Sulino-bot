406b23dd7aa971b9d4c2699d93a396a67363e3dd
Ubaid Ur Rahman <150577020+AlwaysUbaid@users.noreply.github.com>
2024-11-02 03:58:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
+  server: {
+    port: 3000
   }
 });
